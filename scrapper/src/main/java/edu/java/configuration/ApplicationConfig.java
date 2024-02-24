49529195a36ea4fc16b3837891cd4ee54e8570ba
stukenvitalii <stukenvitalii@gmail.com>
2024-02-24 13:12:59 +0300

    modify Scheduler

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -4,12 +4,14 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
     @NotNull
+    @Bean
     Scheduler scheduler,
     @NotEmpty String gitHubBaseUrl,
     @NotEmpty String stackOverFlowBaseUrl) {
