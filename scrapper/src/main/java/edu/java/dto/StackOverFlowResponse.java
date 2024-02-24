70135db048cec5b3b66fe5ef8e6b2092a5abf3bf
stukenvitalii <stukenvitalii@gmail.com>
2024-02-24 13:11:40 +0300

    add JsonProperty("items")

--- scrapper/src/main/java/edu/java/dto/StackOverFlowResponse.java
+++ scrapper/src/main/java/edu/java/dto/StackOverFlowResponse.java
@@ -2,6 +2,7 @@ package edu.java.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +10,6 @@ import lombok.Setter;
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class StackOverFlowResponse {
+    @JsonProperty("items")
     private List<StackOverFlowQuestion> items;
 }
