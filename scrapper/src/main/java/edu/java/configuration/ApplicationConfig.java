b1e65c4f6e00329248326d6e0e2ad6de9c7f45bc
stukenvitalii <stukenvitalii@gmail.com>
2024-02-29 21:24:44 +0300

    little fix

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -1,6 +1,5 @@
 package edu.java.configuration;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
