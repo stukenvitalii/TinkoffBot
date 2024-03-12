b43ab1df0b30e7ec7105e7b92ca2608e71a71e89
stukenvitalii <stukenvitalii@gmail.com>
2024-03-12 15:29:08 +0300

    add BotClient

---
+++ scrapper/src/main/java/edu/java/client/BotClient.java
@@ -0,0 +1,32 @@
+package edu.java.client;
+
+import edu.java.model.request.LinkUpdateRequest;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+public class BotClient {
+    private final String baseUrl = "http://localhost:8090";
+
+    private final WebClient webClient;
+
+    public BotClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public String updateLink(String url, List<Long> tgChatIds) throws MalformedURLException {
+        LinkUpdateRequest linkUpdateRequest = new LinkUpdateRequest(1L, new URL(url), "Обновление ссылки", tgChatIds);
+
+        return webClient
+            .post()
+            .uri(baseUrl + "/updates")
+            .body(Mono.just(linkUpdateRequest), LinkUpdateRequest.class)
+            .header("Accept", "application/json")
+            .retrieve()
+            .bodyToMono(String.class)
+            .block();
+    }
+
+}
