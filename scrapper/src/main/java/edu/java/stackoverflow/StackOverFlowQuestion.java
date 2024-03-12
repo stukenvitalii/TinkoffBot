eac7ff19ce5c28bc48084f5e448bcfa71388ab34
stukenvitalii <stukenvitalii@gmail.com>
2024-03-12 17:32:10 +0300

    add Timestamp last activity

--- scrapper/src/main/java/edu/java/stackoverflow/StackOverFlowQuestion.java
+++ scrapper/src/main/java/edu/java/stackoverflow/StackOverFlowQuestion.java
@@ -1,9 +1,15 @@
 package edu.java.stackoverflow;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Getter
 @Setter
@@ -17,4 +23,11 @@ public class StackOverFlowQuestion {
 
     @JsonProperty("title")
     private String title;
+
+    @JsonProperty("last_activity_date")
+    private Long last_activity;
+
+    public Timestamp getLastActivityAsTimestamp() {
+        return Timestamp.valueOf(Instant.ofEpochMilli(last_activity*1000).atZone(ZoneId.systemDefault()).toLocalDateTime());
+    }
 }
