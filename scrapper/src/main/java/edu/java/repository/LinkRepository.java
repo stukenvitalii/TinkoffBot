0675280715f4003b9543291bf689685b168973c3
stukenvitalii <stukenvitalii@gmail.com>
2024-03-17 14:38:27 +0300

    add feature to get comment count and answer count

--- scrapper/src/main/java/edu/java/repository/LinkRepository.java
+++ scrapper/src/main/java/edu/java/repository/LinkRepository.java
@@ -3,6 +3,7 @@ package edu.java.repository;
 import edu.java.model.dto.Link;
 import java.sql.Timestamp;
 import java.util.List;
+import edu.java.model.dto.LinkSof;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
@@ -12,35 +13,50 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class LinkRepository {
     private final JdbcClient jdbcClient;
-    private final String lastCheckTimeString = "lastCheckTime";
 
     @Transactional
-    public void add(Link entity) {
+    public int add(Link entity) {
+        try {
+            String sql =
+                "insert into link(url, chat_id, last_check_time,created_at) "
+                    + "values(:url,:chatId,:lastCheckTime,:createdAt)";
 
-        String sql =
-            "insert into link(url, chat_id, last_check_time,created_at) values(:url,:chatId,:lastCheckTime,:createdAt)";
+            jdbcClient.sql(sql)
+                .param("url", entity.getUrl().toString())
+                .param("chatId", entity.getChatId())
+                .param("lastCheckTime", entity.getLastCheckTime())
+                .param("createdAt", entity.getCreatedAt())
+                .update();
 
-        jdbcClient.sql(sql)
-            .param("url", entity.getUrl())
-            .param("chatId", entity.getChatId())
-            .param(lastCheckTimeString, entity.getLastCheckTime())
-            .param("createdAt", entity.getCreatedAt())
-            .update();
+        } catch (Exception e) {
+            return -1;
+        }
+        return 1;
     }
 
     @Transactional
-    public void remove(Long id) {
-
-        String sql = "delete from link where id = ?";
-        int count = jdbcClient.sql(sql).param(1, id).update();
-        if (count == 0) {
-            throw new RuntimeException("link not found");
+    public int remove(Long id) {
+        try {
+            String sql = "delete from link where id = ?";
+            int count = jdbcClient.sql(sql).param(1, id).update();
+            if (count == 0) {
+                throw new RuntimeException("link not found");
+            }
+        } catch (RuntimeException e) {
+            return -1;
         }
+        return 1;
     }
 
     @Transactional(readOnly = true)
     public List<Link> findAll() {
         String sql = "select * from link";
+        return jdbcClient.sql(sql).query(Link.class).list();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Link> findUnUpdatedLinks() {
+        String sql = "select * from link where EXTRACT(SECOND FROM (now() -last_check_time )) > 30";
         return jdbcClient.sql(sql).query(Link.class).list();
     }
 
@@ -50,13 +66,35 @@ public class LinkRepository {
             "update link set  last_check_time = (:lastCheckTime) where id = :link_id";
         jdbcClient.sql(sql)
             .param("link_id", id)
-            .param(lastCheckTimeString, lastCheckTime)
+            .param("lastCheckTime", lastCheckTime)
             .update();
     }
 
     @Transactional(readOnly = true)
-    public List<Link> getUnUpdatedLinks() {
-        String sql = "select * from link where EXTRACT(SECOND FROM (now() - last_check_time )) > 30";
-        return jdbcClient.sql(sql).query(Link.class).list();
+    public LinkSof getLinkPropertiesById(Long id) {
+        String sql = "select * from sof_link_properties where link_id = ? ";
+        return jdbcClient.sql(sql).param(1, id).query(LinkSof.class).single();
     }
+
+    @Transactional
+    public void updateCountOfCommentsById(Long id, Long count) {
+        String sql =
+            "update sof_link_properties set comment_count = ? where link_id = ?";
+        jdbcClient.sql(sql)
+            .param(1, count)
+            .param(2, id)
+            .update();
+    }
+
+    @Transactional
+    public void updateCountOfAnswersById(Long id, Long count) {
+        String sql =
+            "update sof_link_properties set answer_count = ? where link_id = ?";
+        jdbcClient.sql(sql)
+            .param(1, count)
+            .param(2, id)
+            .update();
+    }
+
 }
+
