f55e7f2114344d5202d9a5233da436915a780eab
stukenvitalii <stukenvitalii@gmail.com>
2024-03-17 19:40:30 +0300

    add jooq custom config

---
+++ scrapper/src/main/java/edu/java/configuration/Config.java
@@ -0,0 +1,17 @@
+package edu.java.configuration;
+
+import org.jooq.conf.RenderQuotedNames;
+import org.jooq.impl.DefaultConfiguration;
+import org.springframework.boot.autoconfigure.jooq.*;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class Config {
+    @Bean
+    public DefaultConfigurationCustomizer configurationCustomizer() {
+        return (DefaultConfiguration c) -> c.settings()
+            .withRenderQuotedNames(
+                RenderQuotedNames.EXPLICIT_DEFAULT_UNQUOTED
+            );
+    }
+}
