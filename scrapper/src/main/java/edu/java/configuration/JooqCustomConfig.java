aa95c582370c3631d06aeb37c6cb7312c5a3eb15
stukenvitalii <stukenvitalii@gmail.com>
2024-04-03 22:41:23 +0300

    rename

--- scrapper/src/main/java/edu/java/configuration/JooqCustomConfig.java
+++ scrapper/src/main/java/edu/java/configuration/JooqCustomConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class Config {
+public class JooqCustomConfig {
     @Bean
     public DefaultConfigurationCustomizer configurationCustomizer() {
         return (DefaultConfiguration c) -> c.settings()
