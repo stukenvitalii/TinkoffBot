8eab7cc3519ed693fc97077244fa2af75a6977a4
stukenvitalii <stukenvitalii@gmail.com>
2024-04-03 22:36:27 +0300

    add KafkaConfiguration for bot module

---
+++ bot/src/main/java/edu/java/bot/configuration/kafka/KafkaConfiguration.java
@@ -0,0 +1,33 @@
+package edu.java.bot.configuration.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfiguration {
+    @Value("${app.kafka.topics}")
+    String topics;
+
+    @Value("${app.kafka.partitions-num}")
+    int partitionsNum;
+
+    @Value("${app.kafka.replicas-num}")
+    int replicasNum;
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name(topics)
+            .partitions(partitionsNum)
+            .replicas(replicasNum)
+            .build();
+    }
+
+    @KafkaListener(id = "myId", topics = "topic1") //TODO fix?
+    public void listen(String in) {
+        System.out.println(in);
+    }
+}
