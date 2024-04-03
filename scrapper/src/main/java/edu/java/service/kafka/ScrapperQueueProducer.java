30cc260e4318192312283e4a56c17795e12bd63d
stukenvitalii <stukenvitalii@gmail.com>
2024-04-04 00:33:00 +0300

    add ConditionalOnProperty for ScrapperQueueProducer bean

--- scrapper/src/main/java/edu/java/service/kafka/ScrapperQueueProducer.java
+++ scrapper/src/main/java/edu/java/service/kafka/ScrapperQueueProducer.java
@@ -1,14 +1,15 @@
 package edu.java.service.kafka;
 
 import edu.java.model.request.LinkUpdateRequest;
-import lombok.RequiredArgsConstructor;
+import edu.java.service.sender.SenderService;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ScrapperQueueProducer {
+@ConditionalOnProperty(value = "app.use-queue", havingValue = "true", matchIfMissing = true)
+public class ScrapperQueueProducer implements SenderService {
     private final NewTopic topic;
 
     private final KafkaTemplate<String, String> template;
@@ -18,7 +19,8 @@ public class ScrapperQueueProducer {
         this.template = template;
     }
 
-    public void send() {
+    @Override
+    public void updateLink(LinkUpdateRequest linkUpdateRequest) {
         template.send(topic.name(), "scrapper queue producer");
     }
 }
