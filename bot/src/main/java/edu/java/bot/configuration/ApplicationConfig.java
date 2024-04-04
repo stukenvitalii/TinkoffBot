5a27578ea5d12de9222ef7f53649a10fbc38165d
stukenvitalii <stukenvitalii@gmail.com>
2024-04-04 13:14:05 +0300

    configure Kafka for Bot module

--- bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
@@ -1,9 +1,15 @@
 package edu.java.bot.configuration;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
-public record ApplicationConfig(@NotEmpty String telegramToken) {}
+public record ApplicationConfig(@NotEmpty String telegramToken, @NotNull
+Kafka kafka) {
+    public record Kafka(@NotNull String topics, @NotNull int partitionsNum, @NotNull int replicasNum) {
+    }
+}
+
