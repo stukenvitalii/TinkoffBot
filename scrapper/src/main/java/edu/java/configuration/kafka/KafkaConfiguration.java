afc6a3b2a356fed87c5f5337fe8acfdbaea7ea9a
stukenvitalii <stukenvitalii@gmail.com>
2024-04-07 19:33:42 +0300

    remove unnecessary code

--- scrapper/src/main/java/edu/java/configuration/kafka/KafkaConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/kafka/KafkaConfiguration.java
@@ -2,11 +2,9 @@ package edu.java.configuration.kafka;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration
 public class KafkaConfiguration {
@@ -26,12 +24,4 @@ public class KafkaConfiguration {
             .replicas(replicasNum)
             .build();
     }
-
-    @Bean
-    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
-        return args -> {
-            template.send("topic1", "test"); //TODO fix?
-        };
-    }
-
 }
